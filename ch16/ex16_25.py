5468cc55757936d0dd715e6fb4c9022ce6d1d32b
bobk48 <bobk48@gmail.com>
2018-04-26 12:40:59 -0700

    Source code change for Example 16.25

--- ch16/ex16_25.py
+++ ch16/ex16_25.py
@@ -1,21 +1,16 @@
 #!/usr/local/bin/python
 import os
 import sys
-if len(sys.argv) = = 1 or len(sys.
-argv) > 2:    #check for no or too many arg(s)
-    print "Usage: ", sys.argv[0], "is not an ordinary file"
+if len(sys.argv) == 1 or len(sys.argv) > 2:  #check for no/too many args 
+    print "Usage: ", sys.argv[0], " ordinary file"
     sys.exit(1)
-if sys.argv[1].startswith('.'):     #added check for dot files
-    print "Usage: ", sys.argv[1], "is a dot file"
-    sys.exit(1)
-if os.path.isfile(sys.
-argv[1]):                      #bingo, get stats
+if os.path.isfile(sys.argv[1]):      #bingo, get stats
     filename = sys.argv[1]
     fileinfo = os.stat(filename)
     print "Filename   inode    size"
-    Print " "
+    print " "
     print filename, fileinfo.st_ino, fileinfo.st_size
     sys.exit(0)
-else:                          # argument must be a directory
+else:                                # argument must something else!
     print sys.argv[1], " argument must be an ordinary file"
     sys.exit(1)
--- ch16/ex16_25.sh
+++ ch16/ex16_25.sh
@@ -1,17 +1,18 @@
 #!/bin/csh
-if ( ($#argv == 0) | | ($#argv > 1) ) then
+if ( ( $#argv == 0 ) || ( $#argv > 1 ) ) then
     echo "Usage: $0 ordinary_file"
     exit 1
 endif
-if ( -f $argv[1] ) then
+if ( -f $1 ) then
     set filename = $argv[1]
-    set fileinfo = 'ls -il $filename'
+    set fileinfo = `ls -il $filename`
     set inode = $fileinfo[1]
-    set size = $fileinfo[5]
-    echo "Name       Inode      Size"
-    echo
-    echo "$filename     $inode    $size"
+    set size = $fileinfo[6]
+    echo "File Name:        $filename"
+    echo "Inode Number:     $inode"
+    echo "Size (bytes):     $size" 
     exit 0
+else
+    echo "$0: argument must be an ordinary file" 
+    exit 1
 endif
-echo "$0: argument must be an ordinary file"
-exit 1
